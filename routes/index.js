4b258bdf27712b2c6fdc0f4007ceae30bf37e6d1
junseo_lee <dlwnstj99@ajou.ac.kr>
2023-02-28 21:13:44 +0900

    FIRST COMMIT

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Express");
+  res.send("THis is nodemon");
 });
 
 module.exports = router;
