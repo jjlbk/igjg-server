bfe3fb2961ea57c18d00bb96f3e186d9e35256ce
KimSumin <ajou456@ajou.ac.kr>
2023-03-02 16:14:30 +0900

    feat: implement inserting datas without keywords
    This requires a seperate serviceAccountKey.

---
+++ routes/inserting.js
@@ -0,0 +1,47 @@
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+const serviceAccount = require("./serviceAccountKey.json"); // WARN
+initializeApp({
+  credential: cert(serviceAccount),
+});
+const db = getFirestore();
+
+insertDatasToPolices = (datas) => {
+  for (let data of datas) {
+    const docRef = db.collection("policies").doc();
+    docRef.set({
+      title: data.title,
+      registrationDate: Timestamp.fromDate(new Date(data.date)),
+      department: data.dept,
+      contentURL: data.url,
+      contentHTML: data.contentHTML,
+      keywords: ["", ""],
+      region: data.region,
+    });
+  }
+};
+
+insertDatasToNews = (datas) => {
+  for (let data of datas) {
+    const docRef = db.collection("news").doc();
+    docRef.set({
+      title: data.title,
+      registrationDate: Timestamp.fromDate(new Date(data.date)),
+      thumbnailURL: data.thumbnailURL,
+      contentURL: data.url,
+      contentHTML: data.contentHTML,
+      keywords: ["", ""],
+      region: data.region,
+    });
+  }
+};
+
+module.exports = { insertDatasToPolices, insertDatasToNews };
