936677caab35d87bbc3a6daa1ccf1382e715c1ba
KimSumin <ajou456@ajou.ac.kr>
2023-03-20 16:39:36 +0900

    fix: move serviceAccounKey

--- routes/inserting.js
+++ routes/inserting.js
@@ -1,17 +1,8 @@
 const {
-  initializeApp,
-  applicationDefault,
-  cert,
-} = require("firebase-admin/app");
-const {
   getFirestore,
   Timestamp,
   FieldValue,
 } = require("firebase-admin/firestore");
-const serviceAccount = require("./serviceAccountKey.json"); // WARN
-initializeApp({
-  credential: cert(serviceAccount),
-});
 const db = getFirestore();
 
 var mod = require("korean-text-analytics");
